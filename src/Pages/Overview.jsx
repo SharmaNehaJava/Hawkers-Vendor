f1757de8d8d6aecc4925d6c6f8197c75a1dcee7c
Sujal Jindal <70793100+sujaljindal@users.noreply.github.com>
2024-11-09 10:53:26 +0530

    UI Components made

--- src/Pages/Overview.jsx
+++ src/Pages/Overview.jsx
@@ -1,50 +1,40 @@
-import React from 'react'
+import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
+import { motion } from "framer-motion";
 
-const AddItems = () => {
-  return (
-    <>
-    <div className='h-20  px-4 flex justify-between items-center'>
+import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
+import SalesOverviewChart from "../components/overview/SalesOverviewChart";
+import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
+import SalesChannelChart from "../components/overview/SalesChannelChart";
 
-      <div className='bg-white px-4 py-2 justify-center text-center rounded-md'>
-        <h3 className='font-semibold'>Total Sales</h3>
-        <p className='text-blue-500'>1888888</p>
-      </div>
+const OverviewPage = () => {
+	return (
+		<div className='flex-1 overflow-auto relative z-10'>
+			<Header title='Overview' />
 
-      <div className='bg-white px-4 py-2 justify-center text-center rounded-md' >
-        <h3 className='font-semibold'>New Users</h3>
-        <p className='text-blue-500'>1555</p>
-      </div>
-      
-      <div className='bg-white px-4 py-2 justify-center text-center rounded-md'>
-        <h3 className='font-semibold'>Total Products</h3>
-        <p className='text-blue-500'>15</p>
-      </div>
+			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+				{/* STATS */}
+				<motion.div
+					className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
+					initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ duration: 1 }}
+				>
+					<StatCard name='Total Sales' icon={Zap} value='$12,345' color='#6366F1' />
+					<StatCard name='New Users' icon={Users} value='1,234' color='#8B5CF6' />
+					<StatCard name='Total Products' icon={ShoppingBag} value='567' color='#EC4899' />
+					<StatCard name='Conversion Rate' icon={BarChart2} value='12.5%' color='#10B981' />
+				</motion.div>
 
-      {/* <div className='bg-white px-4 py-2 justify-center text-center rounded-md'>
-        <h3 className='font-semibold'>Conversion rates</h3>
-        <p className='text-blue-500'>123</p>
-      </div> */}
+				{/* CHARTS */}
 
-    </div>
-
-    <div className='h-3/6  flex justify-between'>
-      <div className='text-center w-1/2 p-2 bg-white m-2'>
-        <h3 className='font-medium'>Sales Overview</h3>
-        <div className=' h-5/6'></div>
-      </div>
-      <div className='text-center w-1/2 p-2 bg-white m-2'>
-        <h3  className='font-medium'>Category Distribution</h3>
-        <div className=' h-5/6'></div>
-      </div>
-    </div>
-
-    <div className='text-center h-2/6 p-2 bg-white m-2'>
-      <h3  className='font-medium'>Sales By Channel</h3>
-      <div className='h-5/6'></div>
-    </div>
-    
-    </>
-  )
-}
-
-export default AddItems
+				<div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+					<SalesOverviewChart />
+					<CategoryDistributionChart />
+					<SalesChannelChart />
+				</div>
+			</main>
+		</div>
+	);
+};
+export default OverviewPage;
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,58 +1,41 @@
-import React from 'react'
+import { motion } from "framer-motion";
 
-const Products = () => {
-  return (
-    <>
-    <div className='h-20  px-4 flex justify-between items-center'>
+import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
 
-      <div className='bg-white px-4 py-2 justify-center text-center rounded-md'>
-        <h3 className='font-semibold'>Total Products</h3>
-        <p className='text-blue-500'>15</p>
-      </div>
+import { AlertTriangle, DollarSign, Package, TrendingUp } from "lucide-react";
+import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
+import SalesTrendChart from "../components/products/SalesTrendChart";
+import ProductsTable from "../components/products/ProductsTable";
 
-      <div className='bg-white px-4 py-2 justify-center text-center rounded-md' >
-        <h3 className='font-semibold'>Top Selling</h3>
-        <p className='text-blue-500'>5</p>
-      </div>
-      
-      <div className='bg-white px-4 py-2 justify-center text-center rounded-md'>
-        <h3 className='font-semibold'>Low Stocks</h3>
-        <p className='text-red-500'>1</p>
-      </div>
+const ProductsPage = () => {
+	return (
+		<div className='flex-1 overflow-auto relative z-10'>
+			<Header title='Products' />
 
-      <div className='bg-white px-4 py-2 justify-center text-center rounded-md'>
-        <h3 className='font-semibold'>Total Revenue</h3>
-        <p className='text-blue-500'>12380</p>
-      </div>
+			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
+				{/* STATS */}
+				<motion.div
+					className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
+					initial={{ opacity: 0, y: 20 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ duration: 1 }}
+				>
+					<StatCard name='Total Products' icon={Package} value={1234} color='#6366F1' />
+					<StatCard name='Top Selling' icon={TrendingUp} value={89} color='#10B981' />
+					<StatCard name='Low Stock' icon={AlertTriangle} value={23} color='#F59E0B' />
+					<StatCard name='Total Revenue' icon={DollarSign} value={"$543,210"} color='#EF4444' />
+				</motion.div>
 
-    </div>
+				<ProductsTable />
 
-    <div className='h-3/6 justify-between'>
-      <h3>Product List</h3>
-      {/* add an option to sort by price, sales .. and search option by name*/}
-      <ul className='flex'>
-        <li>Name</li>
-        <li>Category</li>
-        <li>Price</li>
-        <li>Stocks</li>
-        <li>Sales</li>
-        <li>Actions(Edit/Delete)</li>
-      </ul>
-    </div>
-
-    <div className='h-2/6  flex justify-between'>
-      <div className='text-center w-1/2 p-2 bg-white m-2'>
-        <h3 className='font-medium'>Sales Overview</h3>
-        <div className=' h-5/6'></div>
-      </div>
-      <div className='text-center w-1/2 p-2 bg-white m-2'>
-        <h3  className='font-medium'>Category Distribution</h3>
-        <div className=' h-5/6'></div>
-      </div>
-    </div>
-    
-    </>
-  )
-}
-
-export default Products;
+				{/* CHARTS */}
+				<div className='grid grid-col-1 lg:grid-cols-2 gap-8'>
+					<SalesTrendChart />
+					<CategoryDistributionChart />
+				</div>
+			</main>
+		</div>
+	);
+};
+export default ProductsPage;
